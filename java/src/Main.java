85a187ef05187c7ff579490d88ed038468289fe6
Daniel <daniel.wojtalik@gmail.com>
2019-11-21 21:17:17 +0100

    First commit in feature

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,6 +3,6 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First line in master");
+        System.out.println("First line in feature");
     }
 }
