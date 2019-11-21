cb851c3ebfe4c3b0e08d6f6b1079c4f7d96dd5be
Daniel <daniel.wojtalik@gmail.com>
2019-11-21 21:28:31 +0100

    Second commit in master

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("First line in master");
+        System.out.println("Second line in master");
     }
 }
